0b1fd13baf12271621a354d5a06b6efcbae70429
Ikigand <110550982+Ikigand@users.noreply.github.com>
2022-08-03 22:07:27 +0700

    Add files via upload

---
+++ king21.py
@@ -0,0 +1,15 @@
+Penginstallan :
+
+$ apt update && apt upgrade
+$ pkg install python
+$ pkg install bash
+$ pkg install git
+$ git clone https://github.com/Sxp-ID/spam-whatsappv2
+$ cd spam-whatsappv2
+$ bash install.sh
+$ python spam.py
+
+Thanks for watching..
+Jangan lupa untuk subrek channel yt admin FREE TUTORIAL
+dan aktifkan notifikasi loncengnya biar tau kalo
+admin upload video baru !
